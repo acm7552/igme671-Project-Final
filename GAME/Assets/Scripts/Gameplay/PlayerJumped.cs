ba3c161e5c64a254f614b470b117611acfa89676
acm7552 <acm7552@g.rit.edu>
2021-04-07 23:46:34 -0400

    Wednesday commit Milestone 3 work

--- GAME/Assets/Scripts/Gameplay/PlayerJumped.cs
+++ GAME/Assets/Scripts/Gameplay/PlayerJumped.cs
@@ -15,6 +15,7 @@ namespace Platformer.Gameplay
         {
             //if (player.audioSource && player.jumpAudio)
             //    player.audioSource.PlayOneShot(player.jumpAudio);
+            player.gameObject.GetComponent<FMODUnity.StudioEventEmitter>().Play(); // Jump Audio Event
         }
     }
 }
